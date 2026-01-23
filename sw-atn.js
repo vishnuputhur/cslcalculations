f6f3ddc1052e92939e2714da59861bb048f1c2b0
vishnuputhur <vishnuputhur@yahoo.com>
2026-01-23 20:36:26 +0530

    Update sw-atn.js

--- sw-atn.js
+++ sw-atn.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'atn-v3';
+const CACHE_NAME = 'atn-register-v1';
 const ASSETS = [
   'atn.html',
-  'manifest.json',
+  'manifest-atn.json',
   'atn-icon-192.png',
   'icon-512.png'
 ];
@@ -9,5 +9,11 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
